67b9823de8e2948395d832ee15ca9bad07357445
ruchit3601 <ruchitchudasama123@gmail.com>
2021-08-27 09:21:00 +0530

    validation user

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -7,6 +7,10 @@ import passport from "passport";
 // Models
 import { UserModel } from "../../database/user";
 
+// validation
+import { ValidateSignup, ValidateSignin } from "../../validation/auth"
+
+
 const Router  = express.Router();
 
 /*
@@ -18,6 +22,8 @@ Method  POST
 */
 Router.post("/signup", async (req, res) => {
     try {
+        await ValidateSignup(req.body.credentials);
+
         await UserModel.findByEmailAndPhone(req.body.credentials);
         const newUser = await UserModel.create(req.body.credentials);
         const token = newUser.generateJwtToken();
@@ -36,6 +42,8 @@ Method  POST
 */
 Router.post("/signin", async (req, res) => {
     try{
+        await ValidateSignin(req.body.credentials);
+
         const user = await UserModel.findByEmailAndPassword(req.body.credentials);
         const token = user.generateJwtToken();
         return res.status(200).json({ token, status: "success" });
@@ -76,6 +84,5 @@ Router.get(
       return res.json({ token: req.session.passport.user.token });
     }
   );
-
 
 export default Router;
